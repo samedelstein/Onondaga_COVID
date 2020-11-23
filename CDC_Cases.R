1b75747ae5317e565d31400310fd4ecf62181c9d
Sam Edelstein <samedelstein@users.noreply.github.com>
2020-11-23 10:32:22 -0500

    Create CDC_Cases.R
    Added county data from the CDC

---
+++ CDC_Cases.R
@@ -0,0 +1,15 @@
+library(jsonlite)
+library(data.table)
+library(tidyverse)
+library(ggplot2)
+ny_COVID_data_CDC <- read_json('https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=integrated_county_timeseries_state_NY_external')
+ny_COVID_data_CDC_list <- ny_COVID_data_CDC$integrated_county_timeseries_external_data
+lstData <- Map(as.data.frame, ny_COVID_data_CDC_list)
+ny_COVID_data_CDC_df <- rbindlist(lstData) %>%
+  filter(county == 'Onondaga County')
+names(ny_COVID_data_CDC_df)
+ggplot(ny_COVID_data_CDC_df) +
+  geom_line(aes(date, new_deaths_7_day_rolling_average, group = 1))
+
+
+ny_COVID_data_CDC
