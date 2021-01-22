1af7a05d812f1ed69dc2e0e36c32e57d77a71cf5
samedelstein <sam.i.edelstein@gmail.com>
2021-01-21 20:50:22 -0500

    added data

--- city_case_mapping.R
+++ city_case_mapping.R
@@ -7,7 +7,7 @@ city_case_mapping_old <- city_case_mapping_old %>%
   mutate(DATE = case_when(CONFIRMED < 7785 ~ ymd(DATE) - years(1),
                    TRUE ~ ymd(DATE)))
 
-City_case_mapping <- fromJSON(paste0("https://services3.arcgis.com/6QuzuucBh0MLJk7u/arcgis/rest/services/City_case_mapping_",gsub('(\\D)0', '\\1', format(Sys.Date(), "%b_%d")),"/FeatureServer/1/query?f=json&where=OBJECTID%3E1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=4000&resultType=standard&cacheHint=true"))
+City_case_mapping <- fromJSON(paste0("https://services3.arcgis.com/6QuzuucBh0MLJk7u/arcgis/rest/services/City_case_mapping_",gsub('(\\D)0', '\\1', format(Sys.Date(), "%B_%d")),"/FeatureServer/1/query?f=json&where=OBJECTID%3E1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=4000&resultType=standard&cacheHint=true"))
 
 City_case_mapping_df <- City_case_mapping$features$attributes %>%
   mutate(DATE = case_when(CONFIRMED < 7785 ~ as.Date(paste0(DATE, '-', 2020), '%B%d-%Y'),
